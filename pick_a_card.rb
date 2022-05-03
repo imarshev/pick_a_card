a2da799551150a7675faf3174d260b37d065402b
imarshev <imarshev@mail.ru>
2022-05-03 11:22:41 +0300

    Multiple cards

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,18 @@
-#encoding UTF-8
-
 values = ['2','3','4','5','6','7','8','9','10','J', 'Q', 'K', 'A']
 suits = ['♦', '♠', '♥', '♣']
 
-puts "#{values.sample}#{suits.sample}"
+cards = []
+
+values.each do |value|
+  suits.each { |suit| cards << "#{value} #{suit}" }
+end
+
+cards.shuffle!
+
+puts "Сколько карт достать?"
+
+number = gets.to_i
+
+number.times do
+  puts cards.pop
+end
